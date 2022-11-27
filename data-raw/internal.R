44a8a7aef3191f3f50866a7e429865f58ac1d18b
UchidaMizuki <uchidamizuki@vivaldi.net>
2022-11-27 15:23:58 +0900

    Delete functions

--- data-raw/internal.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-
-# internal ----------------------------------------------------------------
-
-gsitiles_photo <- read_rds("data-raw/data-gsi_photo/gsitiles_photo.rds")
-
-usethis::use_data(gsitiles_photo,
-                  internal = TRUE,
-                  overwrite = TRUE)
