0bcbdd77bda32ff25d1af8d86ab1a3eeb1df3f6e
UchidaMizuki <uchidamizuki@vivaldi.net>
2022-11-27 15:50:06 +0900

    Update prefecture data

--- data-raw/data-prefecture.R
+++ data-raw/data-prefecture.R
@@ -1,36 +1,34 @@
 library(tidyverse)
 library(rvest)
-library(mapdata)
+# library(mapdata)
 library(sf)
 
 # data-prefecture ---------------------------------------------------------
+
+JGD2011 <- 6668
 
 pref_name <- read_html("https://www.land.mlit.go.jp/webland/api.html") |>
   html_elements("table.api_table") |>
   nth(5) |>
   html_table() |>
   rename(pref_code = `都道府県コード`,
-         pref_name = `英語表記`,
          pref_name_ja = `日本語表記`) |>
-  relocate(pref_code, pref_name) |>
-  mutate(pref_name = pref_name |>
-           str_extract("^[^\\s]+"))
+  select(pref_code, pref_name_ja)
 
-prefecture <- maps::map("japan",
-                        plot = FALSE,
-                        fill = TRUE) |>
-  st_as_sf() |>
-  st_make_valid() |>
-
-  # https://github.com/r-spatial/sf/issues/951
+prefecture <- rnaturalearth::ne_states("japan",
+                                       returnclass = "sf") |>
   as_tibble() |>
   st_as_sf() |>
-
-  rename(pref_name = ID) |>
-  mutate(pref_name = str_to_title(pref_name)) |>
-
+  st_transform(JGD2011) |>
+  select(iso_3166_2, name) |>
+  mutate(pref_code = iso_3166_2 |>
+           str_extract("(?<=JP-)\\d{2}") |>
+           as.integer()) |>
+  select(!iso_3166_2) |>
+  rename(pref_name = name) |>
+  arrange(pref_code) |>
   left_join(pref_name,
-            by = "pref_name") |>
+            by = "pref_code") |>
   relocate(pref_code, pref_name, pref_name_ja)
 
 usethis::use_data(prefecture,
